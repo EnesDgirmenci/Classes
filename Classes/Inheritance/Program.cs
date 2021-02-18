af85f9267a0197f47b8d90d866e964b48565af27
İbrahim Enes <49075207+EnesDgirmenci@users.noreply.github.com>
2021-02-18 16:56:58 +0300

    Add files via upload
    47. ders videosu sonrası

--- Classes/Inheritance/Program.cs
+++ Classes/Inheritance/Program.cs
@@ -6,9 +6,21 @@ namespace Inheritance
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
-            customer.
+            Person[] persons = new Person[2]
+            {
+                new Customer{FName= "Engin" },
+                new Student{FName="Derin" },
+            };
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FName);
+            }
+            Console.ReadLine();
         }
+    }
+    class Person2
+    {
+
     }
     class Person
     {
@@ -16,9 +28,15 @@ namespace Inheritance
         public string FName { get; set; }
         public string LName { get; set; }
     }
+    //Bir nesneyi sadece bir kere inheritance alabiliriz //interfaceler inheritance gibi çalışır
+    //birden fazla interface implementasyon yapabiliriz  //
     class Customer:Person
     {
         public string City { get; set; }
+
+    }class Student:Person
+    {
+        public string Department { get; set; }
 
     }
 }
